d36cbedf15ac9337c577a64eac2aab93931b2e08
kruth99 <kendallruth99@gmail.com>
2022-12-08 03:10:45 +0000

    Added Artist 4

--- creative-app/front-end/src/pages/Artist4.js
+++ creative-app/front-end/src/pages/Artist4.js
@@ -13,7 +13,7 @@ const Artist4 = () => {
       </div>
       <div class="main-container">
         <div class='container-fluid'>
-          <h1> <br/> Blake Shelton<br/> </h1>
+          <h1> <br/>Kane Brown<br/> </h1>
           <p><br/> Kane Allen Brown[2][3] (born October 21, 1993)[1] is an American singer. Brown first came to the attention of the public through social media.[4] He released his first EP, titled Closer, in June 2015, and followed it with a new single, "Used to Love You Sober", in October 2015. After Brown signed with RCA Nashville in early 2016, the song was included on his EP Chapter 1, released in March 2016. He released his first full-length album, the self-titled Kane Brown, on December 2, 2016. The single "What Ifs" came from this album, and in October 2017, Brown became the first artist to have simultaneous number ones on all five main Billboard country charts.[5] Brown released his second album, Experiment, in November 2018, which became his first number one album on the Billboard 200.[6] <br/> </p>
         </div>
       </div>
